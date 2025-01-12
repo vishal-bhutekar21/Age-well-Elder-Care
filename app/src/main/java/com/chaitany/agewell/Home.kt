41e4281fe6ea5d7ffbd26bf0b99c73683a0d9f0e
Vishal-bhutekekar21 <vishalbhutekar33772@gmail.com>
2025-01-12 18:51:20 +0530

    added home

--- app/src/main/java/com/chaitany/agewell/Home.kt
+++ app/src/main/java/com/chaitany/agewell/Home.kt
@@ -8,15 +8,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Home : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+       setContentView(R.layout.activity_home)
+
     }
 }

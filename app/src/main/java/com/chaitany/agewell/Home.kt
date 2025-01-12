e6a7d63e2bcca11c31b5f4e315de6add237d0e43
Vishal-bhutekekar21 <vishalbhutekar33772@gmail.com>
2025-01-12 22:32:18 +0530

    vishal changes to the main homescreen

--- app/src/main/java/com/chaitany/agewell/Home.kt
+++ app/src/main/java/com/chaitany/agewell/Home.kt
@@ -1,19 +1,65 @@
 package com.chaitany.agewell
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.MenuItem
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class Home : AppCompatActivity() {
+class Home : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-       setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_home)
 
+        // Initialize the DrawerLayout
+        drawerLayout = findViewById(R.id.drawerLayout)
+
+        // Set up the ActionBarDrawerToggle
+        toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // Enable hamburger button click
+        findViewById<ImageButton>(R.id.menuButton).setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        // Set up NavigationView listener
+        val navigationView: NavigationView = findViewById(R.id.navigationView)
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_home -> Toast.makeText(this, "Home selected", Toast.LENGTH_SHORT).show()
+            R.id.nav_profile -> Toast.makeText(this, "Profile selected", Toast.LENGTH_SHORT).show()
+            R.id.nav_logout -> Toast.makeText(this, "Logged out", Toast.LENGTH_SHORT).show()
+            R.id.nav_share -> Toast.makeText(this, "Share selected", Toast.LENGTH_SHORT).show()
+            R.id.nav_feedback -> Toast.makeText(this, "Feedback selected", Toast.LENGTH_SHORT).show()
+            R.id.nav_about -> Toast.makeText(this, "About selected", Toast.LENGTH_SHORT).show()
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }

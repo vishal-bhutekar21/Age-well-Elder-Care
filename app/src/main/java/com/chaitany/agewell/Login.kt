9109ddf80dcb753ca1b5a08f4afc67051a05af72
Chaitany <chaitnykakde517@gmail.com>
2025-01-12 15:16:48 +0530

    commit after login page

--- app/src/main/java/com/chaitany/agewell/Login.kt
+++ app/src/main/java/com/chaitany/agewell/Login.kt
@@ -1,28 +1,16 @@
 package com.chaitany.agewell
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Splash : AppCompatActivity() {
-    override     fun onCreate(savedInstanceState: Bundle?) {
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-
-        val handler = Handler()
-
-        // Navigate to LoginActivity after 2 seconds (2000 milliseconds)
-        handler.postDelayed({
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
-            finish() // Finish the SplashActivity so it won't stay in the back stack
-        }, 2000) // 2000ms delay
-
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
